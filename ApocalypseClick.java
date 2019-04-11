643cea588df3f4fcd3924da661c8d15dd56799bb
spencerdow1 <47456150+spencerdow1@users.noreply.github.com>
2019-04-10 20:27:52 -0600

    Add files via upload

--- ApocalypseClick.java
+++ ApocalypseClick.java
@@ -27,8 +27,8 @@ public class ApocalypseClick implements EventHandler<MouseEvent> {
     private Coord WhitePieceMove;
     private Coord BlackPieceSelection;
     private Coord BlackPieceMove;
-    private AIPlayer computer = new AIPlayer(0);
-    GameDynamics gameDynamics = new GameDynamics();
+    private AIPlayer computer = new AIPlayer(1);
+    GameDynamicsGUI gameDynamics = new GameDynamicsGUI();
     GameSet gameSet = new DefaultGameSet();
     Board board = new Board(gameSet);
     ArrayList<Piece> list = gameSet.getGamePieces();
@@ -60,21 +60,85 @@ public class ApocalypseClick implements EventHandler<MouseEvent> {
         // set the team turn maybe?
 
         // Logic to send the clicks to the right location
-        if (numTurnClicks == 1) {
-        	setHighlightedPiece(clickCoord);
-            setWhitePieceSelection(clickCoord);
+        if(0 == 1) {
+            if (numTurnClicks == 1) {
+                setHighlightedPiece(clickCoord);
+                setWhitePieceSelection(clickCoord);
 
-        
-       
+
+                updateGui(gameDynamics, gameSet, board);
+
+
+            } else if (numTurnClicks == 2) {
+                // we can re-set turn clicks back to zero here
+                setWhitePieceMove(clickCoord);
+
+                unhighlightAll();
+
+
+                // re-set the turn clicks and carry out the turn //
+                // this is where we can actually carry out the simultaneous
+                // movement in the game
+
+                // increment the turn
+                numTurn += 1;
+                System.out.println("Turn number " + numTurn);
+
+                // set the movement list that the game would use
+
+
+                // this is just convenience to display in console, later
+                // change to actual movement
+
+
+                System.out.println("");
+                // re-set the turn clicks to start a new turn
+                numTurnClicks = 0;
+                root.getChildren().clear();
+                gameDynamics.moveWithAI(board, getWhitePieceSelection(), getWhitePieceMove(),
+                        list, gameSet, computer);
+                updateGui(gameDynamics, gameSet, board);
+                if (gameDynamics.winCondition(gameSet, board) == "draw" || gameDynamics.winCondition(gameSet, board) == "black"
+                        || gameDynamics.winCondition(gameSet, board) == "white") {
+                    System.out.print(gameDynamics.winCondition(gameSet, board));
+                    System.exit(0);
+
+                }
+            }
+        }
+        else{
+            if (numTurnClicks == 1) {
+                setHighlightedPiece(clickCoord);
+                setWhitePieceSelection(clickCoord);
+
+
+                updateGui(gameDynamics, gameSet, board);
+
+
+            } else if (numTurnClicks == 2) {
+                // we can re-set turn clicks back to zero here
+                setWhitePieceMove(clickCoord);
+
+                unhighlightAll();
+                updateGui(gameDynamics, gameSet, board);
+            }
+        else if (numTurnClicks == 3){
+            setHighlightedPiece(clickCoord);
+            setBlackPieceSelection(clickCoord);
             updateGui(gameDynamics, gameSet, board);
-            
 
-        } else if (numTurnClicks == 2) {
-            // we can re-set turn clicks back to zero here
-            setWhitePieceMove(clickCoord);
-            
-            unhighlightAll();
-            
+
+            }
+
+            else {
+            // note we leave this here because we might re-set the number
+            // of turn clicks if the (future) move is not valid to 2
+            setBlackPieceMove(clickCoord);
+
+            }
+
+
+            if (numTurnClicks == 4){
 
             // re-set the turn clicks and carry out the turn //
             // this is where we can actually carry out the simultaneous
@@ -82,64 +146,19 @@ public class ApocalypseClick implements EventHandler<MouseEvent> {
 
             // increment the turn
             numTurn += 1;
-            System.out.println("Turn number " + numTurn);
+            unhighlightAll();
+
+            System.out.println("Turn number "+numTurn);
 
             // set the movement list that the game would use
-
-
-            // this is just convenience to display in console, later
-            // change to actual movement
-
 
             System.out.println("");
             // re-set the turn clicks to start a new turn
             numTurnClicks = 0;
             root.getChildren().clear();
-            gameDynamics.moveWithAI(board, getWhitePieceSelection(), getWhitePieceMove(),
-                    list, gameSet, computer);
+            gameDynamics.moveWithUserInput(board, getWhitePieceSelection(), getWhitePieceMove(),
+                        getBlackPieceSelection(), getBlackPieceMove(), list, gameSet);
             updateGui(gameDynamics, gameSet, board);
-            if (gameDynamics.winCondition(gameSet, board) == "draw" || gameDynamics.winCondition(gameSet, board) == "black"
-                    || gameDynamics.winCondition(gameSet, board) == "white") {
-                System.out.print(gameDynamics.winCondition(gameSet, board));
-                System.exit(0);
-
-            }
-        }
-        /*else if (numTurnClicks == 3){
-            setBlackPieceSelection(clickCoord);
-            myGame.setBlackPieceSelection(clickCoord);
-        }
-        else {
-            // note we leave this here because we might re-set the number
-            // of turn clicks if the (future) move is not valid to 2
-            setBlackPieceMove(clickCoord);
-            myGame.setBlackPieceMove(clickCoord);
-        }
-
-        if (numTurnClicks == 4){
-
-            // re-set the turn clicks and carry out the turn //
-            // this is where we can actually carry out the simultaneous
-            // movement in the game
-            
-            // increment the turn
-            numTurn += 1;
-            System.out.println("Turn number "+numTurn);
-
-            // set the movement list that the game would use
-            myGame.setAllMoves();
-
-            // this is just convenience to display in console, later
-            // change to actual movement
-            for (Coord aMove : myGame.getAllMoves()){
-                System.out.println(aMove.toString());
-            }
-            
-            System.out.println("");
-            // re-set the turn clicks to start a new turn
-            numTurnClicks = 0;
-                root.getChildren().clear();
-                updateGui(gameDynamics, gameSet, board);
             if(gameDynamics.winCondition(gameSet, board) == "draw" || gameDynamics.winCondition(gameSet , board) == "black"
                     || gameDynamics.winCondition(gameSet , board) == "white"){
                 System.out.print(gameDynamics.winCondition(gameSet, board));
@@ -147,188 +166,190 @@ public class ApocalypseClick implements EventHandler<MouseEvent> {
 
             }
         }
-
-    }*/
+        }
     }
 
-    public ArrayList<Piece> getList() {
-        return list;
-    }
+            public ArrayList<Piece> getList () {
+                return list;
+            }
 
-    public void setList(ArrayList<Piece> list) {
-        this.list = list;
-    }
+            public void setList (ArrayList < Piece > list) {
+                this.list = list;
+            }
 
-    public void setRoot(GridPane root){
-        this.root = root;
-    }
+            public void setRoot (GridPane root){
+                this.root = root;
+            }
 
-    public void setWhitePieceSelection(Coord whitePieceSelection) {
-        WhitePieceSelection = whitePieceSelection;
-    }
+            public void setWhitePieceSelection (Coord whitePieceSelection){
+                WhitePieceSelection = whitePieceSelection;
+            }
 
-    public Coord getWhitePieceSelection() {
-        return WhitePieceSelection;
-    }
+            public Coord getWhitePieceSelection () {
+                return WhitePieceSelection;
+            }
 
-    public void setWhitePieceMove(Coord whitePieceMove) {
-        WhitePieceMove = whitePieceMove;
-    }
+            public void setWhitePieceMove (Coord whitePieceMove){
+                WhitePieceMove = whitePieceMove;
+            }
 
-    public Coord getWhitePieceMove() {
-        return WhitePieceMove;
-    }
+            public Coord getWhitePieceMove () {
+                return WhitePieceMove;
+            }
 
-    public void setBlackPieceSelection(Coord blackPieceSelection) {
-        BlackPieceSelection = blackPieceSelection;
-    }
+            public void setBlackPieceSelection (Coord blackPieceSelection){
+                BlackPieceSelection = blackPieceSelection;
+            }
 
-    public Coord getBlackPieceSelection() {
-        return BlackPieceSelection;
-    }
+            public Coord getBlackPieceSelection () {
+                return BlackPieceSelection;
+            }
 
-    public void setBlackPieceMove(Coord blackPieceMove) {
-        BlackPieceMove = blackPieceMove;
-    }
+            public void setBlackPieceMove (Coord blackPieceMove){
+                BlackPieceMove = blackPieceMove;
+            }
 
-    public Coord getBlackPieceMove() {
-        return BlackPieceMove;
-    }
+            public Coord getBlackPieceMove () {
+                return BlackPieceMove;
+            }
 
-    // we don't want to have to worry about a constructor which will produce a
-    // copy of the game for the given state, we are not concerned about
-    // privacy leaks here. We're all consenting adults here.
-
-
-    public void setLastClick(int x, int y){
-        lastClick = new Coord(x, y);
-    }
+            // we don't want to have to worry about a constructor which will produce a
+            // copy of the game for the given state, we are not concerned about
+            // privacy leaks here. We're all consenting adults here.
 
 
-    public Coord getLastClick(){
-        Coord copyCoord = new Coord(lastClick);
-        return copyCoord;
-    }
+            public void setLastClick ( int x, int y){
+                lastClick = new Coord(x, y);
+            }
 
 
-    public void setXSize(int size){
-        sceneSizeX = size;        
-    }
+            public Coord getLastClick () {
+                Coord copyCoord = new Coord(lastClick);
+                return copyCoord;
+            }
 
 
-    public int getXSize(){
-        int copySize = sceneSizeX;
-        return copySize;
-    }
+            public void setXSize ( int size){
+                sceneSizeX = size;
+            }
 
 
-    public void setYSize(int size){
-        sceneSizeY = size;
-    }
+            public int getXSize () {
+                int copySize = sceneSizeX;
+                return copySize;
+            }
 
 
-    public int getYSize(){
-        int copySize = sceneSizeY;
-        return copySize;
-    }
+            public void setYSize ( int size){
+                sceneSizeY = size;
+            }
 
 
-    public void updateGui(GameDynamics gameDynamics, GameSet gameSet, Board board){
-            list = gameSet.getGamePieces();
-        int count = 0;
-        int rowNum = 5;
-        int colNum = 5;
-        for (int row = 0; row < rowNum; row++) {
-            for (int col = 0; col < colNum; col++) {
-                Rectangle rec = new Rectangle();
-                if (count == 0 ) {
-                    rec.setWidth(60);
-                    rec.setHeight(60);
-                    rec.setFill(Color.DARKRED);
-                    count ++;
+            public int getYSize () {
+                int copySize = sceneSizeY;
+                return copySize;
+            }
 
-                }else if (count==1) {
-                    rec.setWidth(60);
-                    rec.setHeight(60);
-                    rec.setFill(Color.GREY);
-                    count = count -1;
+
+            public void updateGui (GameDynamicsGUI gameDynamics, GameSet gameSet, Board board){
+                list = gameSet.getGamePieces();
+                int count = 0;
+                int rowNum = 5;
+                int colNum = 5;
+                for (int row = 0; row < rowNum; row++) {
+                    for (int col = 0; col < colNum; col++) {
+                        Rectangle rec = new Rectangle();
+                        if (count == 0) {
+                            rec.setWidth(60);
+                            rec.setHeight(60);
+                            rec.setFill(Color.DARKRED);
+                            count++;
+
+                        } else if (count == 1) {
+                            rec.setWidth(60);
+                            rec.setHeight(60);
+                            rec.setFill(Color.GREY);
+                            count = count - 1;
+
+                        }
+                        GridPane.setRowIndex(rec, row);
+                        GridPane.setColumnIndex(rec, col);
+
+                        root.getChildren().addAll(rec);
+
+                    }
+                }
+
+
+                Image imageBlackPawn = new Image("GUI/black_pawn.png");
+                Image imageWhitePawn = new Image("GUI/white_pawn.png");
+                Image imageBlackKnight = new Image("GUI/black_knight.png");
+                Image imageWhiteKnight = new Image("GUI/white_knight.png");
+                Image imageWhitePawnHighlight = new Image("GUI/white_pawn_highlight.png");
+                Image imageBlackPawnHighlight = new Image("GUI/black_pawn_highlight.png");
+                Image imageWhiteKnightHighlight = new Image("GUI/white_Knight_highlight.png");
+                Image imageBlackKnightHighlight = new Image("GUI/black_knight_highlight.png");
+
+                for (int l = 0; l < 5; l++) {
+
+                    for (int j = 0; j < 5; j++) {
+
+                        for (Piece aPiece : list) {
+                            Coord checkCoord = new Coord(l, j);
+                            if (aPiece.getPosition().equals(checkCoord)) {
+                                if (aPiece.getType() == "pawn") {
+                                    if (aPiece.getColour() == "white") {
+                                        if (aPiece.isHighlighted()) {
+                                            root.add(new ImageView(imageWhitePawnHighlight), l, abs(4 - j));
+                                        } else {
+                                            root.add(new ImageView(imageWhitePawn), l, abs(4 - j));
+                                        }
+                                    } else{
+                                        if (aPiece.isHighlighted()) {
+                                            root.add(new ImageView(imageBlackPawnHighlight), l, abs(4 - j));
+                                        }
+                                        else root.add(new ImageView(imageBlackPawn), l, abs(4 - j));
+                                    }
+                                }
+                                if (aPiece.getType() == "knight") {
+                                    if (aPiece.getColour() == "white") {
+                                        if (aPiece.isHighlighted()) {
+                                            root.add(new ImageView(imageWhiteKnightHighlight), l, abs(4 - j));
+                                        } else {
+                                            root.add(new ImageView(imageWhiteKnight), l, abs(4 - j));
+                                        }
+
+                                    } else{
+                                        if (aPiece.isHighlighted()) {
+                                            root.add(new ImageView(imageBlackKnightHighlight), l, abs(4 - j));
+                                        }
+                                        else root.add(new ImageView(imageBlackKnight), l, abs(4 - j));
+                                    }
+                                }
+                            }
+
+                        }
+                    }
 
                 }
-                GridPane.setRowIndex(rec, row);
-                GridPane.setColumnIndex(rec, col);
+                setList(list);
+            }
+            public void setHighlightedPiece (Coord pieceCoordinate){
+                Piece selectedPieceWhite = gameSet.getPieceByCoord(pieceCoordinate);
+                selectedPieceWhite.setHighlighted(true);
+            }
 
-                root.getChildren().addAll(rec);
+            public void unhighlightAll ()
+            {
+                for (Piece aPiece : list) {
+                    aPiece.setHighlighted(false);
+                }
+            }
+
+            public static void setOnAction (Object ClickGame){
+                // TODO Auto-generated method stub
 
             }
+
+
         }
-
-
-        Image imageBlackPawn = new Image("GUI/black_pawn.png");
-        Image imageWhitePawn = new Image("GUI/white_pawn.png");
-        Image imageBlackKnight = new Image("GUI/black_knight.png");
-        Image imageWhiteKnight = new Image("GUI/white_knight.png");
-        Image imageWhitePawnHighlight = new Image ( "GUI/white_pawn_highlight.png");
-        Image imageBlackPawnHighlight = new Image ( "GUI/black_pawn_highlight.png");
-        Image imageWhiteKnightHighlight = new Image ( "GUI/white_Knight_highlight.png");
-        Image imageBlackKnightHighlight = new Image ( "GUI/black_knight_highlight.png");
-
-         for (int l = 0; l < 5; l++){
-
-             for (int j = 0; j < 5; j++){
-        
-                 for (Piece aPiece : list){
-                     Coord checkCoord = new Coord(l,j);
-                     if(aPiece.getPosition().equals(checkCoord)){
-                         if(aPiece.getType() == "pawn"){
-                             if(aPiece.getColour() == "white"){
-                            	 if (aPiece.isHighlighted())
-                            	 {
-                            		 root.add(new ImageView(imageWhitePawnHighlight), l, abs(4-j));
-                            	 }else
-                            	 {
-                            		 root.add(new ImageView(imageWhitePawn), l, abs(4-j));
-                            	 }
-                             }
-                             else root.add(new ImageView(imageBlackPawn), l,abs(4-j));
-                         }
-                         if(aPiece.getType() == "knight"){
-                             if(aPiece.getColour() == "white"){
-                            	 if (aPiece.isHighlighted())
-                            	 {
-                            		 root.add(new ImageView(imageWhiteKnightHighlight), l, abs(4-j));
-                            	 }else
-                            	 {
-                            		 root.add(new ImageView(imageWhiteKnight), l, abs(4-j));
-                            	 }
-                                 
-                             }
-                             else root.add(new ImageView(imageBlackKnight), l, abs(4-j));
-                         }
-                     }
-
-                 }
-             }
-
-         }
-         setList(list);
-     }
-    public void setHighlightedPiece(Coord pieceCoordinate) {
-    	Piece selectedPieceWhite = gameSet.getPieceByCoord(pieceCoordinate);
-    	selectedPieceWhite.setHighlighted(true);
-    }
-
-    public void unhighlightAll()
-    {
-    	for (Piece aPiece:list)
-    	{
-    		aPiece.setHighlighted(false);
-    	}
-    }
-
-	public static void setOnAction(Object ClickGame) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-    }
